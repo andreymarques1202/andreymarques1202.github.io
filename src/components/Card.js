a5f18818a5d578dbbc618c30ff906f95d3d82486
Andrey Marques <andreymarques822@gmail.com>
2023-11-28 12:16:14 -0300

    Frontend: Correção do card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const card = () => {
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Web Development</h5>
-        <p class="card-text">Encontre aqui meus projetos</p>
+        <p class="card-text">Encontre aqui meus projetos no github</p>
         <a href="https://github.com/andreymarques1202" class="btn btn-primary">Projects</a>
       </div>
     </div>
