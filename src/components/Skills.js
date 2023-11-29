f449b3c9c8ba8c129e4d37f2640a85ac1d611f12
Andrey Marques <andreymarques822@gmail.com>
2023-11-29 10:22:08 -0300

    Frontend: ajustando icones

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,126 +5,128 @@ const Skills = () => {
     return(
         <div className="container">
             <div className="row">
-            <div className="col-sm-2">
+            
                 
-                    <div className="card-body">
+                    <ul>
+                    <li className="card-body">
                     <i class="devicon-html5-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-css3-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-git-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-github-original colored"></i>
-                    </div>
-                </div>
+                    </li>
+                
             
             <div className="card-titles">
                 <h2>Linguagens de Programação</h2>
             </div>
-            <div className="col-sm-2">
+            
                 
-                    <div className="card-body">
+                    <li className="card-body">
                     <i class="devicon-javascript-plain colored"></i>
-                    </div>
-                </div>
-            
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-nodejs-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-php-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-python-plain colored"></i>
-                    </div>
-                </div>
+                    </li>
+                
             
             <div className="card-titles">
                 <h2>Frameworks</h2>
             </div>
-            <div className="col-sm-2">
+            
                 
-                    <div className="card-body">
+                    <li className="card-body">
                     <i class="devicon-bootstrap-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-bulma-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-react-original colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-laravel-plain colored"></i>
-                    </div>
-                </div>
+                    </li>
+                
             
             <div className="card-titles">
                 <h2>Banco de dados</h2>
             </div>
-            <div className="col-sm-2">
+            
                 
-                    <div className="card-body">
+                    <li className="card-body">
                     <i class="devicon-mysql-plain colored"></i>
-                    </div>
-                </div>
-            
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
+            
+            
+                
+                    <li className="card-body">
                     <i class="devicon-sqlite-plain colored"></i>
-                    </div>
-                </div>
-           
-            <div className="col-sm-2">
+                    </li>
                 
-                    <div className="card-body">
-                    <i class="devicon-postgresql-plain colored"></i>
-                    </div>
-                </div>
+           
             
-            <div className="col-sm-2">
-                    <div className="card-body">
+                
+                    <li className="card-body">
+                    <i class="devicon-postgresql-plain colored"></i>
+                    </li>
+                
+            
+            
+                    <li className="card-body">
                     <i class="devicon-mongodb-plain colored"></i>
-                    </div>
-            </div>
+                    </li>
+                    </ul>
+            
         </div>
         </div>
     )
