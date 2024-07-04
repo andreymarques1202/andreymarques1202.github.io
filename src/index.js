df30a9599d5b53ae902e792fa117326cfb613ca5
Andrey Marques de Marques <97910870+andreymarques1202@users.noreply.github.com>
2024-07-03 21:52:14 -0300

    modo manutenção index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Fragment>
-            <About />
+            <h1 style="text-align: center">Em manutenção! em breve teremos atualização por aqui</h1><br>
+            <p>Contato: (51)99730-3001</p><br>
             <h1>Minhas Skills</h1>
             <hr />
             <Skills/>
